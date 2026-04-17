6370977009bf1c0fbad5da87dcd291cb12b617ac
jerrypm <jerrypurnamamaulid@gmail.com>
2026-04-17 19:39:35 +0700

    feat: wire AppDelegate with keyboard and menu bar modules

--- mac-keyboard-bar/AppDelegate.swift
+++ mac-keyboard-bar/AppDelegate.swift
@@ -3,15 +3,35 @@ import Cocoa
 final class AppDelegate: NSObject, NSApplicationDelegate {
 
     // MARK: - Properties
-    // (populated by Builders in Task 19)
+
+    private var keyboardModule: KeyboardBuilder.Module?
+    private var menuBarModule: MenuBarBuilder.Module?
+    private let accessibility = AccessibilityService()
 
     // MARK: - Lifecycle
 
     func applicationDidFinishLaunching(_ notification: Notification) {
-        // Task 19 wires here
+        let keyboard = KeyboardBuilder.build(
+            accessibility: accessibility,
+            injector: KeyInjectionService(),
+            positionStore: WindowPositionService()
+        )
+        let menuBar = MenuBarBuilder.build(keyboardRouter: keyboard.router)
+
+        keyboardModule = keyboard
+        menuBarModule = menuBar
+
+        promptAccessibilityIfNeeded()
     }
 
     func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
         true
+    }
+
+    // MARK: - Private
+
+    private func promptAccessibilityIfNeeded() {
+        guard !accessibility.isTrusted else { return }
+        accessibility.requestPermission()
     }
 }
